320d90a0eef80323b6dbc27e9d4c7e7680103b15
shifa-ui <shifasaifi8888@gmail.com>
2026-02-25 09:49:07 +0530

    FIRST COMMIT

---
+++ script.js
@@ -0,0 +1,13 @@
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num =  false;
+console.log(num);
+console.log(typeof(num));
+num = "javascript";
+console.log(num);
+console.log(typeof(num));
+var num;
+console.log(num);
+console.log(typeof(num));
+
