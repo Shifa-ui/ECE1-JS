a3ecc06ddc49790664b708f87382c07aca159a26
shifa-ui <shifasaifi8888@gmail.com>
2026-02-25 10:27:47 +0530

    symbol

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-var num = 10;
-console.log(num);
-console.log(typeof(num));
-num =  false;
-console.log(num);
-console.log(typeof(num));
-num = "javascript";
-console.log(num);
-console.log(typeof(num));
-var num;
-console.log(num);
-console.log(typeof(num));
-
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+// num =  false;
+// console.log(num);
+// console.log(typeof(num));
+// num = "javascript";
+// console.log(num);
+// console.log(typeof(num));
+// var num;
+// console.log(num);
+// console.log(typeof(num));
+const sym1 = Symbol(4);
+  console.log(sym1);
+  const sym2 = Symbol(4);
+  console.log(sym2);
+  if(sym1==sym2){
+    console.log("true")
+  }
+  else{
+    console.log("false")
+  }
