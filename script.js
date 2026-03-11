280d54a6fc78fca2ce9b54cfec9f1830a832099e
shifa-ui <shifasaifi8888@gmail.com>
2026-03-11 09:44:56 +0530

    loops

--- script.js
+++ script.js
@@ -10,13 +10,31 @@
 // var num;
 // console.log(num);
 // console.log(typeof(num));
-const sym1 = Symbol(4);
-  console.log(sym1);
-  const sym2 = Symbol(4);
-  console.log(sym2);
-  if(sym1==sym2){
-    console.log("true")
-  }
-  else{
-    console.log("false")
-  }
+// var count;
+// document.write("starting loop" + "<br/>");
+// for(count=0; count<10;count++){
+//   document.write("current count" + count);
+//   document.write("<br/>");
+// }
+// document.write("loop stopped");
+
+//   const person = {
+//     fname:"ajay",
+//     lname:"singh",
+//     age:25
+//   };
+  // for(let x in person){
+  //   console.log("person details"+x+":"+person[x])
+  // }
+  // var age  = 20;
+  // if(age > 18){
+  //   document.write("<b> you are eligible to cast the vote. </b>");
+  // }
+  // let age = 20;
+  // if(age >=18){
+  //   document.write("you are an adult");
+  // }else{
+  //   document.write("you are a minor");
+  // }
+  // var grade = 'A';
+  // document.write("entering switch block")
