3ab8a14caace3b127c6d48632e91ba47bdcdaeca
shifa-ui <shifasaifi8888@gmail.com>
2026-03-18 13:03:46 +0530

    domJS

---
+++ dom.js
@@ -0,0 +1,14 @@
+// const result=document.getElementById("js");
+// result.innerHTML="<b>Hello World</b>";
+// const result=document.getElementsByClassName("p1");
+// console.log(result);
+// const result=document.getElementById("js").getAttribute("class");
+// console.log(result);
+// const x= document.getElementById("p1").innerHTML=result;
+// function myFunction(){
+//     document.getElementById("myH1").setAttribute("class","red");
+// }
+// const heading = document.createElement("h2");
+// const bodytag = document.getElementsByTagName("body")[0];
+// bodytag[0].append(heading);
+// heading.innerText = "hello"; heading.style.backgroundColor="red";
