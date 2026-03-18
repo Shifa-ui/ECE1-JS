0bbd7b57f34c30a6044f7c044a03122386a5624f
shifa-ui <shifasaifi8888@gmail.com>
2026-03-18 10:30:15 +0530

    eventJS

---
+++ event.js
@@ -0,0 +1,21 @@
+function onClick(){
+    console.log("clicked");
+}
+function onMouseover(){
+    console.log("on mouse over");
+}
+function onMouseout(){
+    console.log("on mouse out");
+}
+function onMousedown(){
+    console.log("on mouse down");
+}
+function onMouseup(){
+    console.log("on mouse up");
+}
+function onMousemove(){
+    console.log("on mouse move");
+}
+function onDblclick(){
+    console.log("on double click");
+}
